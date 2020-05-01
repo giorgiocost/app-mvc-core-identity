252f4da2f47b5ba8019c5a0d264c7eed9cb8231e
Giorgio Costa <gegiorgio1991@gmail.com>
2020-04-30 23:28:01 -0300

    Authorize e AllowAnonymous

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,16 +5,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using app_mvc_core_identity.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace app_mvc_core_identity.Controllers
 {
+     [Authorize]
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
 
+        // [Authorize]
         public IActionResult Privacy()
         {
             return View();
